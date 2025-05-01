0f75c94eb7a7cb10bbf55746d5e3974b1bc0bd91
Priangshu Mukherjee <157562695+Priangshu-18@users.noreply.github.com>
2025-05-01 11:19:09 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -84,7 +84,7 @@ async function fetchData(type = "skills") {
     type === "skills" ?
         response = await fetch("skills.json")
         :
-        response = await fetch("./projects/projects.json")
+        response = await fetch("./projects.json");
     const data = await response.json();
     return data;
 }
@@ -107,7 +107,7 @@ function showSkills(skills) {
 function showProjects(projects) {
     let projectsContainer = document.querySelector("#work .box-container");
     let projectHTML = "";
-    projects.slice(0, 10).filter(project => project.category != "android").forEach(project => {
+    projects.forEach(project => {
         projectHTML += `
         <div class="box tilt">
       <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
